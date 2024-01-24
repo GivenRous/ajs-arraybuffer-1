37a6cdc0b5870ca2d69aa7e359c50d40678fb6f2
Ivan Surovegin <givenrous@yandex.ru>
2024-01-24 12:14:58 +0300

    решение

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,15 @@
-import sum from '../basic';
+import Magician from "../magician";
+import Daemon from "../demon";
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+test("Not Stoned", () => {
+  const hero = new Magician();
+  hero.distance = 3;
+  expect(hero.getAttack()).toBe(80);
+});
 
-  expect(result).toBe(6);
+test("Stoned", () => {
+  const hero = new Daemon();
+  hero.distance = 3;
+  hero.setStoned(true);
+  expect(hero.getAttack()).toBe(72);
 });
